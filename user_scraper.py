5038d1accc7b93925752620480020887232c1e61
Urhengulas <joheihe@web.de>
2019-09-30 23:50:59 +0200

    Simplify user_Scraper

--- user_scraper.py
+++ user_scraper.py
@@ -1,15 +1,39 @@
 import logging
 
-from twitterscraper.user_scraper import UserScraper
+import twint
+
 from twitterscraper.scraper_manager import ScraperManager
+from twitterscraper.utils import get_conf
+
+
+def scrape(user_name: str) -> twint.user.user:
+    conf = get_conf(user_name)
+    user = scrape_user(conf)
+    user.followers_list = scrape_follows_list(twint.run.Followers, conf)
+    user.following_list = scrape_follows_list(twint.run.Following, conf)
+
+    return user
+
+
+def scrape_user(conf: twint.Config) -> twint.user.user:
+    twint.run.Lookup(conf)
+    user = twint.output.users_list.pop()
+    return user
+
+
+def scrape_follows_list(func, conf: twint.Config) -> list:
+    func(conf)
+
+    ret = twint.output.follows_list
+    twint.output.follows_list = []
+    return ret
 
 
 class Scraper(ScraperManager):
     @staticmethod
     def scrape(user_name: str):
         logging.info(f"scrape user {user_name}")
-        user_scraper = UserScraper(user_name)
-        user = user_scraper.scrape()
+        user = scrape(user_name)
         return user
 
 
@@ -25,9 +49,6 @@ if __name__ == "__main__":
     insert_topic = os.getenv("KAFKA_INSERT_TOPIC", "users_scraped")
     fetch_topic = os.getenv("KAFKA_FETCH_TOPIC", "user_names")
     kafka_host_port = os.getenv("KAFKA_HOST_PORT", "localhost:9092")
-
-    logging.info(
-        f"# ENV VARS\n{insert_topic}\n{fetch_topic}\n{kafka_host_port}")
 
     scraper_manager = Scraper(
         insert_topic=insert_topic,
