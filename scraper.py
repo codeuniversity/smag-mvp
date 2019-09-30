27aee5b13f25dda4cc546fac8ab07fa298edc98e
Urhengulas <joheihe@web.de>
2019-09-30 17:34:13 +0200

    Rm scraper (playground file)

--- scraper.py
+++
@@ -1,7 +0,0 @@
-from twitterscraper.user_scraper import UserScraper
-
-
-if __name__ == "__main__":
-    user_scraper = UserScraper("urhengula5")
-    user_scraper.scrape()
-    print(user_scraper.to_json())
