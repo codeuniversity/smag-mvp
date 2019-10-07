a838d83686144b07028fe14445807bf2aa685524
jo-fr <jonathan@freibergers.de>
2019-10-07 11:06:00 +0200

    fix import

--- api/grpcserver/main/main.go
+++ api/grpcserver/main/main.go
@@ -1,7 +1,9 @@
 package main
 
+import server "github.com/codeuniversity/smag-mvp/api/grpcserver"
+
 func main() {
-	server := NewGrpcServer(10000, "localhost")
+	server := server.NewGrpcServer(10000, "localhost")
 
 	server.Listen()
 
