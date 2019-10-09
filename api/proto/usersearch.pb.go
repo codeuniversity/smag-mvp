4cd321cd66688e30a4bd7c91da1f7dad44063d78
jo-fr <jonathan@freibergers.de>
2019-10-09 14:21:47 +0200

    recompile proto file

--- api/proto/usersearch.pb.go
+++ api/proto/usersearch.pb.go
@@ -40,7 +40,7 @@ func (m *User) Reset()         { *m = User{} }
 func (m *User) String() string { return proto.CompactTextString(m) }
 func (*User) ProtoMessage()    {}
 func (*User) Descriptor() ([]byte, []int) {
-	return fileDescriptor_usersearch_fec7f30762a98884, []int{0}
+	return fileDescriptor_usersearch_0dfaaf7997ced939, []int{0}
 }
 func (m *User) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_User.Unmarshal(m, b)
@@ -120,7 +120,7 @@ func (m *UserSearchResponse) Reset()         { *m = UserSearchResponse{} }
 func (m *UserSearchResponse) String() string { return proto.CompactTextString(m) }
 func (*UserSearchResponse) ProtoMessage()    {}
 func (*UserSearchResponse) Descriptor() ([]byte, []int) {
-	return fileDescriptor_usersearch_fec7f30762a98884, []int{1}
+	return fileDescriptor_usersearch_0dfaaf7997ced939, []int{1}
 }
 func (m *UserSearchResponse) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_UserSearchResponse.Unmarshal(m, b)
@@ -147,38 +147,38 @@ func (m *UserSearchResponse) GetUserList() []*User {
 	return nil
 }
 
-type UserSearchrequest struct {
+type UserSearchRequest struct {
 	UserName             string   `protobuf:"bytes,1,opt,name=user_name,json=userName,proto3" json:"user_name,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
 }
 
-func (m *UserSearchrequest) Reset()         { *m = UserSearchrequest{} }
-func (m *UserSearchrequest) String() string { return proto.CompactTextString(m) }
-func (*UserSearchrequest) ProtoMessage()    {}
-func (*UserSearchrequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_usersearch_fec7f30762a98884, []int{2}
+func (m *UserSearchRequest) Reset()         { *m = UserSearchRequest{} }
+func (m *UserSearchRequest) String() string { return proto.CompactTextString(m) }
+func (*UserSearchRequest) ProtoMessage()    {}
+func (*UserSearchRequest) Descriptor() ([]byte, []int) {
+	return fileDescriptor_usersearch_0dfaaf7997ced939, []int{2}
 }
-func (m *UserSearchrequest) XXX_Unmarshal(b []byte) error {
-	return xxx_messageInfo_UserSearchrequest.Unmarshal(m, b)
+func (m *UserSearchRequest) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_UserSearchRequest.Unmarshal(m, b)
 }
-func (m *UserSearchrequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	return xxx_messageInfo_UserSearchrequest.Marshal(b, m, deterministic)
+func (m *UserSearchRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_UserSearchRequest.Marshal(b, m, deterministic)
 }
-func (dst *UserSearchrequest) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_UserSearchrequest.Merge(dst, src)
+func (dst *UserSearchRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_UserSearchRequest.Merge(dst, src)
 }
-func (m *UserSearchrequest) XXX_Size() int {
-	return xxx_messageInfo_UserSearchrequest.Size(m)
+func (m *UserSearchRequest) XXX_Size() int {
+	return xxx_messageInfo_UserSearchRequest.Size(m)
 }
-func (m *UserSearchrequest) XXX_DiscardUnknown() {
-	xxx_messageInfo_UserSearchrequest.DiscardUnknown(m)
+func (m *UserSearchRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_UserSearchRequest.DiscardUnknown(m)
 }
 
-var xxx_messageInfo_UserSearchrequest proto.InternalMessageInfo
+var xxx_messageInfo_UserSearchRequest proto.InternalMessageInfo
 
-func (m *UserSearchrequest) GetUserName() string {
+func (m *UserSearchRequest) GetUserName() string {
 	if m != nil {
 		return m.UserName
 	}
@@ -188,7 +188,7 @@ func (m *UserSearchrequest) GetUserName() string {
 func init() {
 	proto.RegisterType((*User)(nil), "proto.User")
 	proto.RegisterType((*UserSearchResponse)(nil), "proto.UserSearchResponse")
-	proto.RegisterType((*UserSearchrequest)(nil), "proto.UserSearchrequest")
+	proto.RegisterType((*UserSearchRequest)(nil), "proto.UserSearchRequest")
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -203,8 +203,8 @@ const _ = grpc.SupportPackageIsVersion4
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type UserSearchServiceClient interface {
-	GetAllUsersLikeUsername(ctx context.Context, in *UserSearchrequest, opts ...grpc.CallOption) (*UserSearchResponse, error)
-	GetUserWithUsername(ctx context.Context, in *UserSearchrequest, opts ...grpc.CallOption) (*User, error)
+	GetAllUsersLikeUsername(ctx context.Context, in *UserSearchRequest, opts ...grpc.CallOption) (*UserSearchResponse, error)
+	GetUserWithUsername(ctx context.Context, in *UserSearchRequest, opts ...grpc.CallOption) (*User, error)
 }
 
 type userSearchServiceClient struct {
@@ -215,7 +215,7 @@ func NewUserSearchServiceClient(cc *grpc.ClientConn) UserSearchServiceClient {
 	return &userSearchServiceClient{cc}
 }
 
-func (c *userSearchServiceClient) GetAllUsersLikeUsername(ctx context.Context, in *UserSearchrequest, opts ...grpc.CallOption) (*UserSearchResponse, error) {
+func (c *userSearchServiceClient) GetAllUsersLikeUsername(ctx context.Context, in *UserSearchRequest, opts ...grpc.CallOption) (*UserSearchResponse, error) {
 	out := new(UserSearchResponse)
 	err := c.cc.Invoke(ctx, "/proto.UserSearchService/GetAllUsersLikeUsername", in, out, opts...)
 	if err != nil {
@@ -224,7 +224,7 @@ func (c *userSearchServiceClient) GetAllUsersLikeUsername(ctx context.Context, i
 	return out, nil
 }
 
-func (c *userSearchServiceClient) GetUserWithUsername(ctx context.Context, in *UserSearchrequest, opts ...grpc.CallOption) (*User, error) {
+func (c *userSearchServiceClient) GetUserWithUsername(ctx context.Context, in *UserSearchRequest, opts ...grpc.CallOption) (*User, error) {
 	out := new(User)
 	err := c.cc.Invoke(ctx, "/proto.UserSearchService/GetUserWithUsername", in, out, opts...)
 	if err != nil {
@@ -235,8 +235,8 @@ func (c *userSearchServiceClient) GetUserWithUsername(ctx context.Context, in *U
 
 // UserSearchServiceServer is the server API for UserSearchService service.
 type UserSearchServiceServer interface {
-	GetAllUsersLikeUsername(context.Context, *UserSearchrequest) (*UserSearchResponse, error)
-	GetUserWithUsername(context.Context, *UserSearchrequest) (*User, error)
+	GetAllUsersLikeUsername(context.Context, *UserSearchRequest) (*UserSearchResponse, error)
+	GetUserWithUsername(context.Context, *UserSearchRequest) (*User, error)
 }
 
 func RegisterUserSearchServiceServer(s *grpc.Server, srv UserSearchServiceServer) {
@@ -244,7 +244,7 @@ func RegisterUserSearchServiceServer(s *grpc.Server, srv UserSearchServiceServer
 }
 
 func _UserSearchService_GetAllUsersLikeUsername_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(UserSearchrequest)
+	in := new(UserSearchRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
@@ -256,13 +256,13 @@ func _UserSearchService_GetAllUsersLikeUsername_Handler(srv interface{}, ctx con
 		FullMethod: "/proto.UserSearchService/GetAllUsersLikeUsername",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(UserSearchServiceServer).GetAllUsersLikeUsername(ctx, req.(*UserSearchrequest))
+		return srv.(UserSearchServiceServer).GetAllUsersLikeUsername(ctx, req.(*UserSearchRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
 func _UserSearchService_GetUserWithUsername_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(UserSearchrequest)
+	in := new(UserSearchRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
@@ -274,7 +274,7 @@ func _UserSearchService_GetUserWithUsername_Handler(srv interface{}, ctx context
 		FullMethod: "/proto.UserSearchService/GetUserWithUsername",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(UserSearchServiceServer).GetUserWithUsername(ctx, req.(*UserSearchrequest))
+		return srv.(UserSearchServiceServer).GetUserWithUsername(ctx, req.(*UserSearchRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -297,29 +297,29 @@ var _UserSearchService_serviceDesc = grpc.ServiceDesc{
 }
 
 func init() {
-	proto.RegisterFile("api/proto/usersearch.proto", fileDescriptor_usersearch_fec7f30762a98884)
+	proto.RegisterFile("api/proto/usersearch.proto", fileDescriptor_usersearch_0dfaaf7997ced939)
 }
 
-var fileDescriptor_usersearch_fec7f30762a98884 = []byte{
-	// 314 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x8c, 0x90, 0xcf, 0x4b, 0x02, 0x41,
-	0x14, 0xc7, 0x99, 0xfc, 0x51, 0x3e, 0xa1, 0x6c, 0x3a, 0x34, 0x19, 0x81, 0x78, 0x69, 0xbb, 0x68,
-	0x18, 0x74, 0x8a, 0x20, 0x3a, 0x78, 0x11, 0x0f, 0x8a, 0x74, 0x5c, 0xc6, 0xe5, 0x95, 0x43, 0xe3,
-	0xce, 0x36, 0x33, 0x6b, 0x7f, 0x4f, 0x7f, 0x68, 0x10, 0x6f, 0xd6, 0x74, 0x45, 0x88, 0x4e, 0xfb,
-	0xf6, 0xf3, 0x7d, 0x3f, 0xe6, 0xfb, 0x85, 0xb6, 0xcc, 0x54, 0x3f, 0xb3, 0xc6, 0x9b, 0x7e, 0xee,
-	0xd0, 0x3a, 0x94, 0x36, 0x59, 0xf4, 0x02, 0xe0, 0xb5, 0xf0, 0xe9, 0x7e, 0x33, 0xa8, 0xce, 0x1c,
-	0x5a, 0x7e, 0x09, 0x0d, 0xea, 0x89, 0x53, 0xb9, 0x44, 0xc1, 0x3a, 0x2c, 0x6a, 0x4c, 0x8e, 0x08,
-	0x8c, 0xe5, 0x12, 0x49, 0xb4, 0x28, 0x75, 0x21, 0x1e, 0x14, 0x22, 0x81, 0x20, 0xb6, 0xa0, 0x32,
-	0x57, 0x46, 0x54, 0x02, 0xa6, 0x92, 0x5f, 0x01, 0xc8, 0x95, 0xf4, 0xd2, 0xc6, 0xb9, 0xd5, 0xa2,
-	0x1a, 0x84, 0x46, 0x41, 0x66, 0x56, 0xf3, 0x1b, 0x68, 0xbd, 0x1a, 0xad, 0xcd, 0xa7, 0x4a, 0xdf,
-	0x5c, 0x9c, 0x98, 0x3c, 0xf5, 0xa2, 0xd6, 0x61, 0x51, 0x75, 0x72, 0xb2, 0xe5, 0xcf, 0x84, 0xf9,
-	0x35, 0xac, 0x11, 0xda, 0xdf, 0xce, 0x7a, 0xe8, 0x3c, 0xde, 0xe0, 0xa2, 0xf1, 0x7e, 0x67, 0x67,
-	0x70, 0x2b, 0x0e, 0x3b, 0x95, 0xa8, 0x39, 0x68, 0x16, 0x86, 0x7b, 0xe4, 0xb2, 0x7c, 0x80, 0xfe,
-	0x5d, 0xf7, 0x11, 0x38, 0x15, 0xd3, 0x10, 0xcd, 0x04, 0x5d, 0x66, 0x52, 0x87, 0x3c, 0x5a, 0x87,
-	0xa1, 0x95, 0xf3, 0x82, 0xed, 0xaf, 0x09, 0xc9, 0x8c, 0x94, 0xf3, 0xdd, 0x5b, 0x38, 0xdd, 0xce,
-	0x5b, 0xfc, 0xc8, 0xd1, 0xf9, 0x3f, 0xb3, 0x1c, 0x7c, 0xb1, 0xf2, 0xc8, 0x14, 0xed, 0x4a, 0x25,
-	0xc8, 0xc7, 0x70, 0x3e, 0x44, 0xff, 0xa4, 0x75, 0x78, 0xd6, 0x48, 0xbd, 0x23, 0x15, 0xb4, 0x80,
-	0x8b, 0xd2, 0xe5, 0x9d, 0x3b, 0xed, 0x8b, 0x3d, 0x65, 0xe3, 0xe0, 0x01, 0xce, 0x86, 0xe8, 0x49,
-	0x78, 0x51, 0x7e, 0xf1, 0x8f, 0x5d, 0x65, 0x7f, 0xf3, 0x7a, 0xa8, 0xef, 0x7e, 0x02, 0x00, 0x00,
-	0xff, 0xff, 0xf7, 0x9f, 0xf9, 0xd2, 0x41, 0x02, 0x00, 0x00,
+var fileDescriptor_usersearch_0dfaaf7997ced939 = []byte{
+	// 313 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x8c, 0x90, 0x4f, 0x4b, 0x03, 0x31,
+	0x10, 0xc5, 0x89, 0xfd, 0xa3, 0x9d, 0x82, 0xd6, 0x78, 0x30, 0x56, 0x84, 0xd2, 0x8b, 0xeb, 0xa5,
+	0x95, 0x0a, 0x9e, 0x44, 0x10, 0x0f, 0xbd, 0x94, 0x1e, 0x5a, 0x8a, 0xc7, 0x25, 0x5d, 0x46, 0x1b,
+	0x4c, 0x37, 0x6b, 0x92, 0xad, 0x9f, 0xc7, 0x0f, 0x2a, 0xc8, 0x64, 0x57, 0xdd, 0xa2, 0x88, 0xa7,
+	0x9d, 0xfd, 0xbd, 0xf9, 0x93, 0xf7, 0xa0, 0x2b, 0x33, 0x35, 0xcc, 0xac, 0xf1, 0x66, 0x98, 0x3b,
+	0xb4, 0x0e, 0xa5, 0x4d, 0x56, 0x83, 0x00, 0x78, 0x23, 0x7c, 0xfa, 0xef, 0x0c, 0xea, 0x0b, 0x87,
+	0x96, 0x9f, 0x42, 0x8b, 0x7a, 0xe2, 0x54, 0xae, 0x51, 0xb0, 0x1e, 0x8b, 0x5a, 0xb3, 0x3d, 0x02,
+	0x53, 0xb9, 0x46, 0x12, 0x2d, 0x4a, 0x5d, 0x88, 0x3b, 0x85, 0x48, 0x20, 0x88, 0x1d, 0xa8, 0x2d,
+	0x95, 0x11, 0xb5, 0x80, 0xa9, 0xe4, 0x67, 0x00, 0x72, 0x23, 0xbd, 0xb4, 0x71, 0x6e, 0xb5, 0xa8,
+	0x07, 0xa1, 0x55, 0x90, 0x85, 0xd5, 0xfc, 0x02, 0x3a, 0x8f, 0x46, 0x6b, 0xf3, 0xaa, 0xd2, 0x27,
+	0x17, 0x27, 0x26, 0x4f, 0xbd, 0x68, 0xf4, 0x58, 0x54, 0x9f, 0x1d, 0x7c, 0xf3, 0x7b, 0xc2, 0xfc,
+	0x1c, 0x4a, 0x84, 0xf6, 0xb3, 0xb3, 0x19, 0x3a, 0xf7, 0xbf, 0x70, 0xd1, 0x78, 0xbd, 0xb5, 0x33,
+	0xb8, 0x15, 0xbb, 0xbd, 0x5a, 0xd4, 0x1e, 0xb5, 0x0b, 0xc3, 0x03, 0x72, 0x59, 0x3d, 0x40, 0xff,
+	0xae, 0x7f, 0x0b, 0x9c, 0x8a, 0x79, 0x88, 0x66, 0x86, 0x2e, 0x33, 0xa9, 0x43, 0x1e, 0x95, 0x61,
+	0x68, 0xe5, 0xbc, 0x60, 0x3f, 0xd7, 0x84, 0x64, 0x26, 0xca, 0xf9, 0xfe, 0x25, 0x1c, 0x56, 0xe7,
+	0x5f, 0x72, 0x74, 0xfe, 0xcf, 0x2c, 0x47, 0x6f, 0xac, 0x3a, 0x32, 0x47, 0xbb, 0x51, 0x09, 0xf2,
+	0x29, 0x1c, 0x8f, 0xd1, 0xdf, 0x69, 0x1d, 0x9e, 0x35, 0x51, 0xcf, 0x48, 0x05, 0x2d, 0xe0, 0xa2,
+	0x72, 0x79, 0xeb, 0x4e, 0xf7, 0xe4, 0x17, 0xa5, 0x74, 0x70, 0x03, 0x47, 0x63, 0xf4, 0x24, 0x3c,
+	0x28, 0xbf, 0xfa, 0xc7, 0xae, 0xaa, 0xbf, 0x65, 0x33, 0xd4, 0x57, 0x1f, 0x01, 0x00, 0x00, 0xff,
+	0xff, 0xdc, 0xb0, 0xfe, 0xc0, 0x41, 0x02, 0x00, 0x00,
 }
