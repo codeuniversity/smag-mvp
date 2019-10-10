67d438f79a3735df7080418de72c6c09ebd9671d
Josef <josef.grieb@gmail.com>
2019-10-10 23:00:08 +0200

    add environment variable

--- insta-comments-scraper/main/main.go
+++ insta-comments-scraper/main/main.go
@@ -3,14 +3,11 @@ package main
 import (
 	insta_comments_scraper "github.com/codeuniversity/smag-mvp/insta-comments-scraper"
 	"github.com/codeuniversity/smag-mvp/service"
-	"os"
+	"github.com/codeuniversity/smag-mvp/utils"
 )
 
 func main() {
-	kafkaAddress := os.Getenv("KAFKA_ADDRESS")
-	if kafkaAddress == "" {
-		kafkaAddress = "52.58.171.160:9092"
-	}
+	kafkaAddress := utils.GetStringFromEnvWithDefault("KAFKA_ADDRESS", "52.58.171.160:9092")
 	s := insta_comments_scraper.New(kafkaAddress)
 	service.CloseOnSignal(s)
 	go s.Run()
