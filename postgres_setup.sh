97a7a3a2defe1f0addafe4c8a74c243b03425e59
Josef <josef.grieb@gmail.com>
2019-10-12 14:43:51 +0200

    postgres setup delete command

--- postgres_setup.sh
+++ postgres_setup.sh
@@ -1,3 +1,2 @@
 psql -h localhost -U postgres -w -c "create database instascraper;"
 migrate -database 'postgres://postgres:password@localhost:5432/instascraper?sslmode=disable' -path db/migrations up
-go run cli/main/main.go $1
