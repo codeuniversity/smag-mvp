37d6bc90682f28c54a0c40e3daf1ca5fbcd0690b
jo-fr <jonathan@freibergers.de>
2019-10-16 12:23:26 +0200

    change Debezium Model

--- models/user.go
+++ models/user.go
@@ -78,28 +78,46 @@ type InstaCommentScrapError struct {
 }
 
 type DebeziumTopic struct {
-	After struct {
-		AvatarURL interface{} `json:"avatar_url"`
-		Bio       interface{} `json:"bio"`
-		CrawlTs   interface{} `json:"crawl_ts"`
-		ID        int         `json:"id"`
-		RealName  interface{} `json:"real_name"`
-		UserName  string      `json:"user_name"`
-	} `json:"after"`
-	Before interface{} `json:"before"`
-	Op     string      `json:"op"`
-	Source struct {
-		Connector string      `json:"connector"`
-		Db        string      `json:"db"`
-		Lsn       int         `json:"lsn"`
-		Name      string      `json:"name"`
-		Schema    string      `json:"schema"`
-		Snapshot  string      `json:"snapshot"`
-		Table     string      `json:"table"`
-		TsMs      int         `json:"ts_ms"`
-		TxID      int         `json:"txId"`
-		Version   string      `json:"version"`
-		Xmin      interface{} `json:"xmin"`
-	} `json:"source"`
-	TsMs int `json:"ts_ms"`
+	Schema struct {
+		Type   string `json:"type"`
+		Fields []struct {
+			Type   string `json:"type"`
+			Fields []struct {
+				Type     string `json:"type"`
+				Optional bool   `json:"optional"`
+				Field    string `json:"field"`
+			} `json:"fields,omitempty"`
+			Optional bool   `json:"optional"`
+			Name     string `json:"name,omitempty"`
+			Field    string `json:"field"`
+		} `json:"fields"`
+		Optional bool   `json:"optional"`
+		Name     string `json:"name"`
+	} `json:"schema"`
+	Payload struct {
+		Before interface{} `json:"before"`
+		After  struct {
+			ID        int         `json:"id"`
+			UserName  string      `json:"user_name"`
+			RealName  interface{} `json:"real_name"`
+			AvatarURL interface{} `json:"avatar_url"`
+			Bio       interface{} `json:"bio"`
+			CrawlTs   interface{} `json:"crawl_ts"`
+		} `json:"after"`
+		Source struct {
+			Version   string      `json:"version"`
+			Connector string      `json:"connector"`
+			Name      string      `json:"name"`
+			TsMs      int64       `json:"ts_ms"`
+			Snapshot  string      `json:"snapshot"`
+			Db        string      `json:"db"`
+			Schema    string      `json:"schema"`
+			Table     string      `json:"table"`
+			TxID      int         `json:"txId"`
+			Lsn       int         `json:"lsn"`
+			Xmin      interface{} `json:"xmin"`
+		} `json:"source"`
+		Op   string `json:"op"`
+		TsMs int64  `json:"ts_ms"`
+	} `json:"payload"`
 }
