013a37b57d190af4238eeb48bd3b67c3853d3323
m-lukas <mail@lukasmueller.de>
2019-10-19 15:46:51 +0200

    Remove unused code from twitter_scraper/twitterscraper/utils.py

--- twitter_scraper/twitterscraper/utils.py
+++ twitter_scraper/twitterscraper/utils.py
@@ -4,7 +4,6 @@ import twint
 def get_conf(user_name: str) -> twint.config.Config:
     c = twint.Config()
     c.Username = user_name
-    # c.User_full = True
     c.Store_object = True
     c.Hide_output = True
     return c
