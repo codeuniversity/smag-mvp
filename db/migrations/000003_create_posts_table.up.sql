083833f551bd0cf7b2085482d3dd05145f9056e2
Josef <josef.grieb@gmail.com>
2019-10-25 09:37:07 +0200

    Added caption to posts

--- db/migrations/000003_create_posts_table.up.sql
+++ db/migrations/000003_create_posts_table.up.sql
@@ -4,5 +4,6 @@ CREATE TABLE IF NOT EXISTS posts
     user_id    INTEGER REFERENCES users (id),
     post_id VARCHAR UNIQUE NOT NULL,
     short_code VARCHAR,
-    picture_url VARCHAR
+    picture_url VARCHAR,
+    caption VARCHAR
 );
