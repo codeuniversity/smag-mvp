10ad37e4d7c9699ce820271437b98c01da89c153
Josef <josef.grieb@gmail.com>
2019-10-29 16:59:40 +0100

    changed the command order

--- debezium/kubernetes/kube-start-postgres.sh
+++ debezium/kubernetes/kube-start-postgres.sh
@@ -1,6 +1,6 @@
-jq --arg name "$POSTGRES_USER" --arg password "$POSTGRES_PASSWORD" --arg host "$POSTGRES_HOST" --arg dbname "$POSTGRES_DATABASE" '.config."database.user"=$name | .config."database.password"=$password | .config."database.hostname"=$host | .config."database.dbname"=$dbname' kube-register-postgres.json > kube-register-postgres-secret.json
-
 migrate -database "postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:5432/instascraper?sslmode=disable" -path db/migrations up
+
+jq --arg name "$POSTGRES_USER" --arg password "$POSTGRES_PASSWORD" --arg host "$POSTGRES_HOST" --arg dbname "$POSTGRES_DATABASE" '.config."database.user"=$name | .config."database.password"=$password | .config."database.hostname"=$host | .config."database.dbname"=$dbname' kube-register-postgres.json > kube-register-postgres-secret.json
 
 curl -i -X POST -H "Accept:application/json" \
   -H  "Content-Type:application/json" \
