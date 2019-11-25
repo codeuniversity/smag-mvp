e4fa4c428fdff1370c77d8c1768d90a1cb126719
Lukas Menzel <lukas.menzel@code.berlin>
2019-11-25 11:53:27 +0100

    resolve PR review comments

--- frontend/src/components/BackButton.js
+++ frontend/src/components/BackButton.js
@@ -2,14 +2,12 @@ import React, { Component, useState } from "react";
 import { withRouter, history } from "react-router";
 import backicon from "./img/chevron-left-solid.svg";
 
-function BackButton() {
-  return (
-    <div className="BackButton">
-      <a href="/">
-        <img className="BackButtonImg" src={backicon} width="24px" />
-      </a>
-    </div>
-  );
-}
+const BackButton = () => (
+  <div className="BackButton">
+    <a href="/">
+      <img className="BackButtonImg" src={backicon} width="24px" />
+    </a>
+  </div>
+);
 
 export default BackButton;
