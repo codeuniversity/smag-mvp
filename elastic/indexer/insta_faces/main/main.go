cc123afee026f77d31ac54a5818c95cfb82eea8a
Josef <josef.grieb@gmail.com>
2019-11-26 22:36:06 +0100

    move faces index,mapping to the elastic package

--- elastic/indexer/insta_faces/main/main.go
+++ elastic/indexer/insta_faces/main/main.go
@@ -3,47 +3,17 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"log"
-
-	"github.com/elastic/go-elasticsearch/v7"
-	"github.com/elastic/go-elasticsearch/v7/esutil"
-
+	"github.com/codeuniversity/smag-mvp/elastic"
 	elasticsearch_indexer "github.com/codeuniversity/smag-mvp/elastic/indexer"
 	indexer "github.com/codeuniversity/smag-mvp/elastic/indexer/insta_faces"
 	"github.com/codeuniversity/smag-mvp/kafka/changestream"
 	"github.com/codeuniversity/smag-mvp/models"
 	"github.com/codeuniversity/smag-mvp/service"
 	"github.com/codeuniversity/smag-mvp/utils"
+	"github.com/elastic/go-elasticsearch/v7"
+	"github.com/elastic/go-elasticsearch/v7/esutil"
+	"log"
 )
-
-const indexCreateBody = `
-{
-	"mappings" : {
-			"properties" : {
-				"encoding_vector": {
-					"type": "binary",
-					"doc_values": true
-				},
-				"post_id": {
-					"type": "integer"
-				},
-				"x": {
-					"type": "integer"
-				},
-				"y": {
-					"type": "integer"
-				},
-				"width": {
-					"type": "integer"
-				},
-				"height":{
-					"type": "integer"
-				}
-			}
-	}
-}
-`
-const esIndex = "faces"
 
 func main() {
 	esHosts := utils.GetMultipliesStringsFromEnvDefault("ES_HOSTS", []string{"http://localhost:9200"})
@@ -51,7 +21,7 @@ func main() {
 	groupID := utils.MustGetStringFromEnv("KAFKA_GROUPID")
 	changesTopic := utils.GetStringFromEnvWithDefault("KAFKA_CHANGE_TOPIC", "postgres.public.face_data")
 
-	i := elasticsearch_indexer.New(esHosts, esIndex, indexCreateBody, kafkaAddress, changesTopic, groupID, indexFace)
+	i := elasticsearch_indexer.New(esHosts, elastic.FacesIndex, elastic.FacesIndexMapping, kafkaAddress, changesTopic, groupID, indexFace)
 
 	service.CloseOnSignal(i)
 	waitUntilDone := i.Start()
@@ -79,7 +49,7 @@ func indexFace(client *elasticsearch.Client, m *changestream.ChangeMessage) erro
 	}
 
 	docReader := esutil.NewJSONReader(doc)
-	response, err := client.Index(esIndex,
+	response, err := client.Index(elastic.FacesIndex,
 		docReader,
 		client.Index.WithHuman(),
 		client.Index.WithPretty(),
