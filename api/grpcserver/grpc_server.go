356d78d53af1de2c34064613b1a1b4acbf83a072
alexmorten <alexandrius.martinus@gmail.com>
2019-11-27 19:11:18 +0100

    add SearchSimilarFaces endpoint to grpc_server

--- api/grpcserver/grpc_server.go
+++ api/grpcserver/grpc_server.go
@@ -1,6 +1,7 @@
 package grpcserver
 
 import (
+	"bytes"
 	"context"
 	"database/sql"
 	"fmt"
@@ -12,11 +13,17 @@ import (
 	// required for postgres
 	_ "github.com/lib/pq"
 
+	"encoding/base64"
 	"github.com/codeuniversity/smag-mvp/api/proto"
+
 	"github.com/codeuniversity/smag-mvp/config"
 	"github.com/codeuniversity/smag-mvp/utils"
 	"github.com/minio/minio-go/v6"
 	"google.golang.org/grpc"
+
+	"github.com/codeuniversity/smag-mvp/elastic"
+	"github.com/codeuniversity/smag-mvp/elastic/search/faces"
+	recognition "github.com/codeuniversity/smag-mvp/faces/proto"
 )
 
 //GrpcServer represents the gRPC Server containing the db connection and port
@@ -24,19 +31,23 @@ type GrpcServer struct {
 	grpcPort string
 	db       *sql.DB
 
-	minioClient *minio.Client
-	bucketName  string
-	region      string
+	minioClient        *minio.Client
+	downloadBucketName string
+	region             string
+	imageUploadBucket  string
+
+	facesClient *faces.Client
 }
 
 type scanFunc func(row *sql.Rows) (proto.User, error)
 
 // NewGrpcServer returns initilized gRPC Server
-func NewGrpcServer(grpcPort string, s3Config *config.S3Config, postgresConfig *config.PostgresConfig) *GrpcServer {
+func NewGrpcServer(grpcPort string, s3Config *config.S3Config, imageUploadBucket string, postgresConfig *config.PostgresConfig, esHosts []string, recognitionServiceAddress string) *GrpcServer {
 	s := &GrpcServer{}
 
-	s.bucketName = s3Config.S3BucketName
+	s.downloadBucketName = s3Config.S3BucketName
 	s.region = s3Config.S3Region
+	s.imageUploadBucket = imageUploadBucket
 
 	minioClient, err := minio.New(s3Config.S3Endpoint, s3Config.S3AccessKeyID, s3Config.S3SecretAccessKey, s3Config.S3UseSSL)
 	utils.MustBeNil(err)
@@ -55,6 +66,17 @@ func NewGrpcServer(grpcPort string, s3Config *config.S3Config, postgresConfig *c
 	db, err := sql.Open("postgres", connectionString)
 	utils.PanicIfNotNil(err)
 	log.Println("✅ Postgres connection established")
+
+	con, err := grpc.Dial(recognitionServiceAddress, grpc.WithInsecure())
+	if err != nil {
+		panic(err)
+	}
+	faceReconClient := recognition.NewFaceRecognizerClient(con)
+	esClient := elastic.InitializeElasticSearch(esHosts)
+	s.facesClient = &faces.Client{
+		FaceRecognitionClient: faceReconClient,
+		ESClient:              esClient,
+	}
 
 	s.grpcPort = grpcPort
 	s.db = db
@@ -82,10 +104,10 @@ func (s *GrpcServer) Listen() {
 //GetAllUsersLikeUsername returns a List of users that are like the given username
 func (s *GrpcServer) GetAllUsersLikeUsername(_ context.Context, username *proto.UserNameRequest) (*proto.UserSearchResponse, error) {
 	response := &proto.UserSearchResponse{}
-	rows, err := s.db.Query(`SELECT id,  COALESCE(user_name, '') as user_name, 
+	rows, err := s.db.Query(`SELECT id,  COALESCE(user_name, '') as user_name,
 										COALESCE(real_name, '') as real_name,
-										COALESCE(bio, '') as bio, 
-										COALESCE(avatar_url, '') as avatar_url 
+										COALESCE(bio, '') as bio,
+										COALESCE(avatar_url, '') as avatar_url
 										FROM users WHERE LOWER(user_name) LIKE LOWER($1)`, fmt.Sprintf("%%%s%%", username.UserName))
 	if err != nil {
 		return nil, err
@@ -111,9 +133,9 @@ func (s *GrpcServer) GetUserWithUsername(_ context.Context, username *proto.User
 	u := &proto.User{}
 	log.Println(username)
 
-	err := s.db.QueryRow(`SELECT id, COALESCE(user_name, '') as user_name, 
+	err := s.db.QueryRow(`SELECT id, COALESCE(user_name, '') as user_name,
 									COALESCE(real_name, '') as real_name,
-									COALESCE(bio, '') as bio, 
+									COALESCE(bio, '') as bio,
 									COALESCE(avatar_url, '') as avatar_url
 									FROM users WHERE user_name = $1`, username.UserName).Scan(&u.Id, &u.UserName, &u.RealName, &u.Bio, &u.AvatarUrl)
 	if err != nil {
@@ -162,9 +184,9 @@ func (s *GrpcServer) getRelationsFromUser(query string, userID string, scanFunc 
 func (s *GrpcServer) GetInstaPostsWithUserId(_ context.Context, request *proto.UserIdRequest) (*proto.InstaPostsResponse, error) {
 	res := &proto.InstaPostsResponse{}
 
-	rows, err := s.db.Query(`SELECT id, COALESCE(post_id, '') as post_id, 
-										COALESCE(short_code, '') as short_code, 
-										COALESCE(caption, '') as caption, 
+	rows, err := s.db.Query(`SELECT id, COALESCE(post_id, '') as post_id,
+										COALESCE(short_code, '') as short_code,
+										COALESCE(caption, '') as caption,
 										COALESCE(internal_picture_url, '') as internal_picture_url
 										FROM posts WHERE user_id=$1`, request.UserId)
 	if err != nil {
@@ -197,12 +219,12 @@ func (s *GrpcServer) GetTaggedPostsWithUserId(_ context.Context, request *proto.
 
 	rows, err := s.db.Query(`SELECT posts.id,
 										COALESCE(posts.post_id, '') as post_id,
-										COALESCE(posts.short_code, '') as short_code, 
-										COALESCE(posts.caption, '') as caption, 
+										COALESCE(posts.short_code, '') as short_code,
+										COALESCE(posts.caption, '') as caption,
 										COALESCE(posts.internal_picture_url, '') as internal_picture_url
-										FROM posts  
-										JOIN post_tagged_users 
-										ON posts.id=post_tagged_users.post_id 
+										FROM posts
+										JOIN post_tagged_users
+										ON posts.id=post_tagged_users.post_id
 										WHERE post_tagged_users.user_id=$1`, request.UserId)
 
 	if err != nil {
@@ -247,7 +269,7 @@ func (s *GrpcServer) getURLForPost(object string) (string, error) {
 	reqParams.Set("response-content-disposition", fmt.Sprintf("attachment; filename=\"%s.jpg\"", object))
 
 	// Generates a presigned url which expires in a day.
-	presignedURL, err := s.minioClient.PresignedGetObject(s.bucketName, object, time.Second*60*60, reqParams)
+	presignedURL, err := s.minioClient.PresignedGetObject(s.downloadBucketName, object, time.Second*60*60, reqParams)
 	if err != nil {
 		log.Println(err)
 		return "", err
@@ -255,4 +277,85 @@ func (s *GrpcServer) getURLForPost(object string) (string, error) {
 	fmt.Println("Successfully generated presigned URL", presignedURL)
 	return presignedURL.String(), nil
 
+}
+
+//SearchSimilarFaces to the given base64 encoded image
+func (s *GrpcServer) SearchSimilarFaces(ctx context.Context, request *proto.FaceSearchRequest) (*proto.FaceSearchResponse, error) {
+	imageContent, err := base64.RawStdEncoding.DecodeString(request.Base64EncodedPicture)
+	if err != nil {
+		log.Println(err)
+		return nil, fmt.Errorf("couldn't base64 decode image: %w", err)
+	}
+	imagePath := utils.RandUUIDSeq()
+	_, err = s.minioClient.PutObject(
+		s.imageUploadBucket,
+		imagePath,
+		bytes.NewReader(imageContent),
+		int64(len(imageContent)),
+		minio.PutObjectOptions{},
+	)
+
+	if err != nil {
+		log.Println(err)
+		return nil, fmt.Errorf("failed to upload image to S3: %w", err)
+	}
+
+	presignedURL, err := s.minioClient.PresignedGetObject(s.imageUploadBucket, imagePath, time.Second*60*60, url.Values{})
+	if err != nil {
+		log.Println(err)
+		return nil, fmt.Errorf("coudln't generate presgined URL: %w", err)
+	}
+
+	faces, err := s.facesClient.FindSimilarFacesInImage(presignedURL.String(), 10)
+	if err != nil {
+		log.Println(err)
+		return nil, fmt.Errorf("coudln't find similar faces: %w", err)
+	}
+
+	if len(faces) == 0 {
+		log.Println("no faces found")
+		return &proto.FaceSearchResponse{Faces: nil}, nil
+	}
+
+	postIDs := []int{}
+
+	for _, face := range faces {
+		postIDs = append(postIDs, face.PostID)
+	}
+
+	rows, err := s.db.Query(`SELECT id, COALESCE(internal_picture_url, '') as internal_picture_url FROM posts WHERE id in $1`, postIDs)
+	if err != nil {
+		return nil, fmt.Errorf("failed to query postgres for posts: %w", err)
+	}
+	defer rows.Close()
+
+	signedPostURLs := map[int]string{}
+	for rows.Next() {
+		var postID int
+		var imgPath string
+		rows.Scan(&postID, &imgPath)
+		if imgPath == "" {
+			continue
+		}
+
+		signedURL, err := s.getURLForPost(imgPath)
+		if err != nil {
+			return nil, fmt.Errorf("couldn't generate signed URL to post image: %w", err)
+		}
+		signedPostURLs[postID] = signedURL
+	}
+
+	responseFaces := []*proto.Face{}
+	for _, foundFace := range faces {
+		responseFaces = append(responseFaces, &proto.Face{
+			PostId:       int32(foundFace.PostID),
+			X:            int32(foundFace.X),
+			Y:            int32(foundFace.Y),
+			Width:        int32(foundFace.Width),
+			Height:       int32(foundFace.Height),
+			FullImageSrc: signedPostURLs[foundFace.PostID],
+		})
+	}
+
+	return &proto.FaceSearchResponse{Faces: responseFaces}, nil
 }
