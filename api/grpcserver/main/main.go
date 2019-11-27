7a2f62b424e93fedf38a406debdba675b106d842
alexmorten <alexandrius.martinus@gmail.com>
2019-11-27 19:33:35 +0100

    update grpc server config

--- api/grpcserver/main/main.go
+++ api/grpcserver/main/main.go
@@ -9,12 +9,13 @@ import (
 func main() {
 
 	grpcPort := utils.GetStringFromEnvWithDefault("GRPC_PORT", "10000")
-
+	uploadBucket := utils.MustGetStringFromEnv("S3_UPLOAD_BUCKET_NAME")
+	esHosts := utils.GetMultipleStringsFromEnvWithDefault("ES_HOSTS", []string{"http://localhost:9200"})
+	recognitionServiceAddress := utils.MustGetStringFromEnv("RECOGNITION_SERVICE_ADDRESS")
 	s3Config := config.GetS3Config()
 	postgresConfig := config.GetPostgresConfig()
 
-	s := server.NewGrpcServer(grpcPort, s3Config, postgresConfig)
+	s := server.NewGrpcServer(grpcPort, s3Config, uploadBucket, postgresConfig, esHosts, recognitionServiceAddress)
 
 	s.Listen()
-
 }
