9fe4babb97763d6a65612f93c23aed51b1b07bb1
alexmorten <alexandrius.martinus@gmail.com>
2019-11-28 20:45:39 +0100

    WIP simple face detection request from frontend

--- frontend/src/components/Start.js
+++ frontend/src/components/Start.js
@@ -1,25 +1,32 @@
-import React, { Fragment } from "react";
-import ReactDOM from "react-dom";
-// import axios from "axios";
-import { withRouter } from "react-router";
+import React from "react";
 import { CameraFeed } from "./camera-feed";
 import Title from "./Title";
+import { FaceSearchRequest } from "../protofiles/usersearch_pb";
+import { UserSearchServicePromiseClient } from "../protofiles/usersearch_grpc_web_pb";
 
-// Upload to local seaweedFS instance
 const uploadImage = async file => {
-  const formData = new FormData();
-  formData.append("file", file);
-
-  // Connect to a seaweedfs instance
+  var reader = new FileReader();
+  reader.onloadend = async () => {
+    const dataUrl = reader.result;
+    const base64Data = dataUrl.split(",")[1];
+    console.log(base64Data);
+    const client = new UserSearchServicePromiseClient("http://localhost:4000");
+    const request = new FaceSearchRequest();
+    request.setBase64encodedpicture(base64Data);
+    const response = await client.searchSimilarFaces(request);
+    console.log(response);
+    debugger;
+  };
+  reader.readAsDataURL(file);
 };
 
-function Start() {
+function Start(props) {
   return (
     <div className="body">
       <div className="container">
         <div className="column-center">
           <Title />
-          <p>Take a pictures!</p>
+          <p> Take a pictures! </p>
           <CameraFeed sendFile={uploadImage} />
         </div>
       </div>
@@ -27,4 +34,4 @@ function Start() {
   );
 }
 
-export default withRouter(Start);
+export default Start;
