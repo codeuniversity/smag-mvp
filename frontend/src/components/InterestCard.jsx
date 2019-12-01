bb198d24eecd017106d4eeb4395667928b78bb55
m-lukas <mail@lukasmueller.de>
2019-12-01 13:31:03 +0100

    Make interestcard configurable via props

--- frontend/src/components/InterestCard.jsx
+++ frontend/src/components/InterestCard.jsx
@@ -11,6 +11,21 @@ class InterestCard extends Component {
         "https://www.diabetes.org/sites/default/files/styles/full_width/public/2019-06/Healthy%20Food%20Made%20Easy%20-min.jpg",
         "http://www.islandsinthesun.com/img/home-maldives5.jpg",
         "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/best-running-shoes-lead-02-1567016766.jpg?crop=0.502xw:1.00xh;0.0577xw,0&resize=640:*"
+      ],
+      title: "Favorites",
+      details: [
+        "Food",
+        "Shoes",
+        "Holidays",
+        "Christmas",
+        "CODE",
+        "Santa Clause",
+        "Drugs",
+        "Görli",
+        "Berlin",
+        "Berta",
+        "Cars",
+        "Climate Protection"
       ]
     };
   }
@@ -20,7 +35,10 @@ class InterestCard extends Component {
       <div className="dashboardCard">
         <div className="cardGrid">
           <Slideshow slides={this.state.slides} />
-          <InterestFooter />
+          <InterestFooter
+            title={this.state.title}
+            details={this.state.details}
+          />
         </div>
       </div>
     );
--- frontend/src/components/InterestFooter.jsx
+++ frontend/src/components/InterestFooter.jsx
@@ -1,11 +1,25 @@
 import React, { Component } from "react";
 
 class InterestFooter extends Component {
+  getDetailsString = () => {
+    let detailsString = "";
+
+    this.props.details.map((item, index) => {
+      detailsString += item;
+
+      if (index < this.props.details.length - 1) {
+        detailsString += ", ";
+      }
+    });
+
+    return detailsString;
+  };
+
   render() {
     return (
       <div className="interestFooter">
-        <h2>Favorites</h2>
-        <p>Holiday, Food, Summer</p>
+        <h2>{this.props.title}</h2>
+        <p>{this.getDetailsString()}</p>
       </div>
     );
   }
