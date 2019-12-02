7149856191fc9624a2ead128cd50832da67d92c5
m-lukas <mail@lukasmueller.de>
2019-12-02 10:14:36 +0100

    Add profile picture dashboard card
    Add ProfileCard component to display a card with only an image.
    Additionally, changed layout of dashboard to a 4x2 grid.

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import InterestCard from "../components/InterestCard";
 import "./../Dashboard.css";
+import ProfileCard from "../components/ProfileCard";
 
 class Dashboard extends Component {
   render() {
     return (
       <div className="dashboard">
+        <ProfileCard pictureUrl="https://avatars3.githubusercontent.com/u/17454617?s=460&v=4" />
         <InterestCard />
       </div>
     );
