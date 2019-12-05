25c381d3840816202ed1863776734f859db73524
Urhengulas <johann.hemmann@code.berlin>
2019-12-05 04:55:57 +0100

    Improve UI

--- nlp/frequency-analyzer/main/main.go
+++ nlp/frequency-analyzer/main/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"strconv"
@@ -9,15 +12,16 @@ import (
 )
 
 func main() {
-	if len(os.Args) != 3 {
-		log.Fatal("Num of args isn't 2.\nUsage: go run main.go <user_id:int>")
+	if len(os.Args) != 2 {
+		log.Fatal("Num of args isn't 1.\nUsage: go run main.go <user_id:int>")
 	}
 	userID, err := strconv.Atoi(os.Args[1])
 	if err != nil {
 		log.Fatalf("Unable to convert user_id=%+v to integer.\nsage: go run main.go <user_id:int>", userID)
 	}
 
-	a := analyzer.New([]string{"localhost:9200"})
+	a := analyzer.New([]string{"http://127.0.0.1:9200"})
+	log.Printf("a=%+v", a)
 
 	// TODO: load cities.json
 	jsonFile, err := os.Open("cities.json")
@@ -30,6 +34,7 @@ func main() {
 	if err := json.Unmarshal(byteValue, &cityMap); err != nil {
 		panic(err)
 	}
+	log.Printf("cityMap=%+v", cityMap)
 
 	foundCities := make(map[string]bool)
 	for city, cityTerms := range cityMap {
@@ -37,6 +42,7 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
+		log.Printf("city=%v \t-> foundTerms=%+v", city, foundTerms)
 		// check if there are results for city
 		if len(foundTerms) > 0 {
 			foundCities[city] = true
@@ -44,6 +50,15 @@ func main() {
 			foundCities[city] = false
 		}
 	}
+	log.Printf("foundCities=%+v", foundCities)
 
-	log.Printf("Could identify following cities for user=%v:\n{%v}", userID, foundCities)
+	res := make([]string, 0, len(foundCities))
+	log.Printf("Could identify following cities for user=%v: {", userID)
+	for city, found := range foundCities {
+		if found == true {
+			res = append(res, city)
+			fmt.Printf("  * %v\n", city)
+		}
+	}
+	fmt.Println("}")
 }
