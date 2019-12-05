86994f2024026c410859dccfd1529667c7db5f22
Josef <josef.grieb@gmail.com>
2019-12-05 15:07:20 +0100

    add configs

--- neo4j-create-dump/main/main.go
+++ neo4j-create-dump/main/main.go
@@ -4,10 +4,10 @@ import neo4j_dump "github.com/codeuniversity/smag-mvp/neo4j-create-dump"
 
 func main() {
 
-	groupId := "test"
-	topic := "inserterTopic"
+	groupId := "neo4j-user-inserter"
+	topic := "postgres.public.follows"
 	kafkaAddress := "my-kafka:9092"
-	kafkaChunk := 100
+	kafkaChunk := 200
 
 	importDump := neo4j_dump.New(kafkaAddress, topic, groupId, kafkaChunk)
 
