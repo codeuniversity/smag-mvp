fe1ea6e716a2256d708900ad71ec3b976c50e92a
m-lukas <mail@lukasmueller.de>
2019-12-16 09:34:20 +0100

    Adapt user model for gorm

--- models/user.go
+++ models/user.go
@@ -4,26 +4,24 @@ import "github.com/jinzhu/gorm"
 
 //UserFollowInfo holds the follow graph info, only relating userNames
 type UserFollowInfo struct {
-	gorm.Model
-	UserName   string   `json:"user_name" gorm:"column:user_name"`
-	RealName   string   `json:"real_name" gorm:"column:real_name"`
-	AvatarURL  string   `json:"avatar_url" gorm:"column:avatar_url"`
-	Bio        string   `json:"bio" gorm:"column:bio"`
-	Followers  []string `json:"followers" gorm:"column:followers"`
-	Followings []string `json:"followings" gorm:"column:followings"`
-	CrawlTs    int      `json:"crawl_ts" gorm:"column:crawl_ts"`
+	UserName   string   `json:"user_name"`
+	RealName   string   `json:"real_name"`
+	AvatarURL  string   `json:"avatar_url"`
+	Bio        string   `json:"bio"`
+	Followers  []string `json:"followers"`
+	Followings []string `json:"followings"`
+	CrawlTs    int      `json:"crawl_ts"`
 }
 
 // User is the struct containing all user fields, used for saving users to the database
 type User struct {
 	gorm.Model
-	UID       string  `json:"uid,omitempty"`
-	UserName  string  `json:"user_name,omitempty" gorm:"column:user_name"`
-	RealName  string  `json:"real_name,omitempty" gorm:"column:real_name"`
-	AvatarURL string  `json:"avatar_url,omitempty" gorm:"column:avatar_url"`
-	Bio       string  `json:"bio,omitempty" gorm:"column:bio"`
-	Follows   []*User `json:"follows,omitempty" gorm:"column:follows"`
-	CrawledAt int     `json:"crawled_at,omitempty" gorm:"column:crawl_ts"`
+	UserName  string `gorm:"unique;primary_key"`
+	RealName  string
+	AvatarURL string `gorm:"column:avatar_url"`
+	Bio       string
+	Follows   []*User
+	CrawledAt int `gorm:"column:crawl_ts"`
 }
 
 // Follow is a struct representing the "follows" table in the postgres database
