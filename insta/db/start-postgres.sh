3bc5f213f3700e3911218e0178cf7c7c249b1c14
Urhengulas <johann.hemmann@code.berlin>
2020-03-22 17:04:49 +0100

    Fix insta/db/start-postgres.sh

--- insta/db/start-postgres.sh
+++ insta/db/start-postgres.sh
@@ -3,8 +3,12 @@
 echo "# MIGRATE DATABASE"
 /migrate -database "postgres://postgres:12345678@postgres:5432/${POSTGRES_DB}?sslmode=disable" -path debezium/migrations up
 
+pwd
+
+ls -a 
+
 echo "# PREPARE DEBEZIUM"
 curl -i -X POST -H "Accept:application/json" \
   -H  "Content-Type:application/json" \
   http://connect:8083/connectors/ \
-  -d @debezium/register-postgres.json
+  -d @db/register-postgres.json
